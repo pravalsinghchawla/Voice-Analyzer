d9a378de4605ea8ccdffdd7ff0dae535a72cfa5c
Praval Singh Chawla <chawlapravals@gmail.com>
2025-07-04 16:19:32 +0200

    implemented audio plotting in time domain

--- voiceAnalyzer.py
+++ voiceAnalyzer.py
@@ -1,10 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from scipy.io import wavfile
 
 class VoiceAnalyzer:
     def __init__(self, fileName) -> None:
-
         self.file = fileName
-        self.sampFreq, self.data = wavfile.read(f"Example_files/{fileName}.wav")
+        self.sampFreq, self.data = wavfile.read(f"exampleFiles/{fileName}.wav")
         self.nyquistFreq = self.sampFreq * 0.5
 
         if len(self.data.shape) == 1:
@@ -14,3 +15,17 @@ class VoiceAnalyzer:
 
         self.sampNums = len(self.audio)
         self.audioTime = self.sampNums / self.sampFreq
+
+    def audioPlot(self):
+        timeAxis = np.linspace(0, self.audioTime, self.sampNums) 
+        
+        plt.plot(timeAxis, self.audio)
+        plt.xlabel("Time (s)")
+        plt.ylabel("Relative amplitude")
+        plt.title("Audio in time domain")
+        plt.show()
+
+
+if __name__ == "__main__":
+    exampleWoman = VoiceAnalyzer("Marlene")
+    exampleWoman.audioPlot()
